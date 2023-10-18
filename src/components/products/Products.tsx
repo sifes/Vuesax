57a2301795696e04ede25e56da6c4439a84d26ac
sifes <lonfark@gmail.com>
2023-10-18 14:29:04 +0300

    feat: Products component

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,20 +1,40 @@
 import React from 'react'
-import { Box, Typography, FormGroup, FormControlLabel, Checkbox } from '@mui/material'
-
-interface IProducts { }
-
-export const Products: React.FC<IProducts> = () => {
+import { Grid, Card, CardContent, Typography, CardActions, Button, CardMedia, Box } from '@mui/material'
+import { IProduct } from '@/pages/products'
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+interface IProducts {
+    products: IProduct[]
+}
+export const Products: React.FC<IProducts> = ({ products }) => {
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            borderRadius: '8px',
-            bgcolor: 'secondary.main',
-            boxShadow: ' 0px 2px 8px 0px rgba(0, 0, 0, 0.14)',
-            padding: '25px 32px',
-            flex: '1 0 auto',
-        }}>
-            <Typography variant='h6'>Search hear</Typography>
-        </Box>
+        <Grid container spacing={4} >
+            {products.map(({ title, thumbnail, id, category, brand, price, discountPercentage, rating }) => (
+                <Grid item md={6} xs={12} xl={4}>
+                    <Card sx={{ bgcolor: 'secondary.main', height: 1, display: 'flex', flexDirection: 'column' }} key={id}>
+                        <CardMedia sx={{ height: 0, paddingTop: '56.25%' }} image={thumbnail} title={title} />
+                        <CardContent sx={{ padding: '12px', flex: '1 1 auto', display: 'flex', flexDirection: 'column' }}>
+                            <Box sx={{ display: 'flex', gap: '10px', justifyContent: 'space-between', alignItems: 'center', mb: 1 }}>
+                                <Box sx={{ bgcolor: '#6F64F8', borderRadius: '3px', display: 'flex', alignItems: 'center', padding: '6px', gap: '3px' }}>
+                                    {rating.toFixed(1)}
+                                    <StarBorderIcon sx={{ color: '#fff' }} />
+                                </Box>
+                                <Box sx={{ display: 'flex', gap: '6px', alignItems: 'center', mb: 1 }}>
+                                    <Typography variant='subtitle1' sx={{ fontWeight: 600 }}  >${price}</Typography>
+                                    <Typography sx={{ textDecoration: 'line-through', fontWeight: 300, fontSize: '10px' }} variant='body2'>${(price + price * discountPercentage / 100).toFixed(0)}</Typography>
+                                </Box>
+                            </Box>
+                            <Typography sx={{ mb: '8px', flex: '1 1 auto' }} variant='h6'>{title}</Typography>
+                            <Box sx={{ display: 'flex', gap: '10px', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Typography variant='subtitle2' sx={{ fontWeight: 300, fontSize: '12px' }}>{brand}</Typography>
+                                <Typography variant='subtitle2' sx={{ fontSize: '12px' }}>{category}</Typography>
+                            </Box>
+                        </CardContent>
+                        <CardActions  >
+                            <Button sx={{ width: 1 }} size="small" variant='contained' >Learn More <ArrowRightAltIcon></ArrowRightAltIcon></Button>
+                        </CardActions>
+                    </Card>
+                </Grid>))}
+        </Grid >
     )
 }
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -3,30 +3,45 @@ import { Products } from '@/components/products/Products'
 import { SearchProducts } from '@/components/products/SearchProducts'
 import { Box } from '@mui/material'
 import React from 'react'
+import axios from 'axios';
 
-interface Iindex { }
+interface Iindex {
+    total: number;
+    skip: number;
+    limit: number;
+    products: IProduct[];
+}
+export interface IProduct {
+    brand: string
+    category: string
+    description: string
+    discountPercentage: number
+    id: number
+    images: string[]
+    price: number
+    rating: number
+    stock: number
+    thumbnail: string
+    title: string
+}
 
-const ProductsPage: React.FC<Iindex> = () => {
+
+const ProductsPage: React.FC<Iindex> = (props) => {
+    const { products } = props
     return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: { sm: 'row', xs: 'column' },
-            gap: '30px',
-            padding: { lg: '25px 32px', xs: '20px 10px' },
-        }}>
+        <Box sx={{ display: 'flex', flexDirection: { sm: 'row', xs: 'column' }, gap: '30px', padding: { lg: '25px 32px', xs: '20px 10px' }, }}>
             <Filters />
-            <Box sx={{
-                width: 1,
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '28px'
-            }}>
+            <Box sx={{ width: 1, display: 'flex', flexDirection: 'column', gap: '28px' }}>
                 <SearchProducts />
-                <Products />
+                <Products products={products} />
             </Box>
-
-        </Box>
+        </Box >
     )
 }
 
 export default ProductsPage
+
+export async function getServerSideProps() {
+    const res = await axios.get(`https://dummyjson.com/products`)
+    return { props: { ...res.data } }
+}
