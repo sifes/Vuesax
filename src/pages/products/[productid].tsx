c4c9d359bf7dda55308146634551aea63b461fb2
sifes <lonfark@gmail.com>
2023-10-24 16:05:08 +0300

    feat: product page

--- src/pages/products/[productid].tsx
+++ src/pages/products/[productid].tsx
@@ -3,37 +3,66 @@ import { GetServerSideProps } from 'next'
 import React from 'react'
 import { IProduct } from '.'
 import { CarouselProduct } from '@/ui/CarouselProduct'
-import { Box, Divider, List, ListItem, ListItemText, Stack, Typography, styled } from '@mui/material'
-import ListSubheader from '@mui/material/ListSubheader';
+import { Box, Divider, List, ListItem, ListItemText, Stack, Typography } from '@mui/material'
+import Rating from '@mui/material/Rating';
+
+
 interface ProductPageProps extends IProduct {
 }
 
 
 
 const ProductPage: React.FC<ProductPageProps> = ({ images, description, discountPercentage, id, price, rating, stock, thumbnail, title, ...info }) => {
-    console.log(info)
     return (
-        <Box sx={{ maxWidth: '1540px', margin: '40px auto' }} >
-            <Stack direction={{ xs: 'column', sm: 'row' }}>
-                <Box sx={{ flex: '0 0 25%', height: '100%', bgcolor: 'secondary.main', padding: '20px 40px', borderRadius: '12px' }}>
+        <Box sx={{ maxWidth: '1540px', margin: '20px auto 20px', bgcolor: 'secondary.main', padding: '50px 60px', borderRadius: '12px', minHeight: '100vh' }} >
+            <Stack direction={{ xs: 'column', sm: 'row' }} gap={5} alignItems={'center'} >
+                <Box sx={{ flex: '0 0 30%', height: '100%' }}>
                     <CarouselProduct images={images} />
                 </Box>
-                <Stack>
-                    <Typography>{title}</Typography>
-                    <Box>Rating</Box>
-                    <Divider></Divider>
-                    <Typography>{price}</Typography>
-                    <Typography>{stock}</Typography>
+                <Stack sx={{ flex: '1 1 auto', height: '100%' }} gap={3}>
+                    <Typography variant='h2'>{title}</Typography>
+                    <Stack direction={'row'} alignItems={'center'} gap={0.5}>
+                        <Typography variant='caption' sx={{ fontWeight: 700, fontSize: '20px', marginTop: '4px' }}>{rating}</Typography>
+                        <Rating size='large' value={rating} readOnly precision={0.1} />
+                    </Stack>
+                    <Divider color='#6F64F8' />
+                    <Stack direction={'row'} alignItems={'center'} >
+                        <Typography variant='h6' sx={{ opacity: 0.7 }}>Price: </Typography>
+                        <Stack margin={'0 24px 0 12px'}>
+                            <Typography variant='subtitle1' sx={{ textDecoration: 'line-through', opacity: 0.7 }}>${(price + price * discountPercentage / 100).toFixed(0)}</Typography>
+                            <Typography variant='h5' sx={{ fontWeight: 600, marginLeft: '4px', marginTop: '-4px' }}>${price}</Typography>
+                        </Stack>
+                        <Typography variant='body1' component={'div'} sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            bgcolor: "primary.main",
+                            color: "white",
+                            p: "10px 16px",
+                            borderRadius: 4
+                        }}>Economy {discountPercentage}%</Typography>
+                    </Stack>
+                    <Typography variant='h5'>Availible in stock: {stock}</Typography>
                     <List >
                         {Object.entries(info).map(([key, value]) => (
-                            <ListItem >
-                                <ListSubheader sx={{ bgcolor: 'transparent', color: 'black' }}>{key}</ListSubheader>
-                                <ListItemText primary={value} />
+                            <ListItem sx={{ paddingLeft: 0 }}>
+                                <ListItemText sx={{ flex: '0 0 130px' }} primaryTypographyProps={{
+                                    fontWeight: 300,
+                                    fontSize: '24px',
+                                    textTransform: 'capitalize',
+                                }}>
+                                    {key}
+                                </ListItemText>
+                                <ListItemText primaryTypographyProps={{
+                                    fontWeight: 500,
+                                    fontSize: '24px',
+                                    textTransform: 'capitalize',
+                                }}>{value}</ListItemText>
                             </ListItem>))}
                     </List>
-                    <Divider></Divider>
-                    <Typography>About this items</Typography>
-                    <Typography>{description}</Typography>
+                    <Divider color='#6F64F8' />
+                    <Typography variant='caption'>About this item</Typography>
+                    <Typography variant='h6'>{description}</Typography>
                 </Stack>
             </Stack>
         </Box>
